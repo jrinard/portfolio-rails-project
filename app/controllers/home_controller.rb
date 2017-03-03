6856c3d9bc2a9a7e5784cf5f51630dc625025965
joshua <jrinard@hotmail.com>
2017-03-03 09:54:33 -0800

    devise user setup complete with single admin user

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create! :email => 'admin@email.com', :password => 'password', :password_confirmation => 'password'
