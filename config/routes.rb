ba06bf731d38044fbe488eaa93f5d4115dea2c6c
“Joshua <JoshuaRinard@Joshuas-iMac.local>
2017-04-08 10:12:41 -0700

    styling on projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
 devise_for :users
-root :to => 'home#index'
-resources :about, :only => [:index]
-resources :home
+root :to => 'projects#index'
+# resources :about, :only => [:index]
+# resources :home
 resources :projects
 
 end
